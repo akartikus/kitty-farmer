2b9bea006dfdb42f2cd93e7965fd98052e80b0d5
akartikus <sitraka.razafimamy@gmail.com>
2023-02-04 22:52:17 +0100

    Use Animation player

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,24 +2,41 @@ extends CharacterBody2D
 
 const SPEED = 40.0
 
-func _physics_process(delta):
+var is_animate = true 
+var is_acting = false
+var last_direction = Vector2.ZERO
 	
+func _physics_process(delta):
+	process_movement()
+
+func process_movement():
 	var direction_x = Input.get_axis("ui_left", "ui_right")	
 	var direction_y = Input.get_axis("ui_up", "ui_down")
 	
 	if direction_x:
-		#$animatedSprite.play("sidewalk")
-		#$animatedSprite.flip_h = direction_x > 0
+		is_animate = true
+		$AnimationPlayer.play("sidewalk")
+		$Sprite2D.flip_h = direction_x < 0
 		velocity.x = direction_x * SPEED
 		velocity.y = 0
+		last_direction = Vector2.RIGHT if direction_x > 0 else Vector2.LEFT
 	elif direction_y:
-		#var animation = "downwalk" if direction_y > 0 else "upwalk"
-		#$animatedSprite.play(animation)
+		is_animate = true
+		$AnimationPlayer.play("downwalk" if direction_y > 0 else "upwalk")
+		last_direction = Vector2.DOWN if direction_y > 0 else Vector2.UP
 		velocity.y = direction_y * SPEED
 		velocity.x = 0
-	else:
-		#$animatedSprite.play("idle")
+	else:	
+		if is_animate:	
+			is_animate = false
+			$AnimationPlayer.pause()
+			if not is_acting:
+				$IdleTimer.start(1)
 		velocity.x = 0
 		velocity.y = 0
-
+		
 	move_and_slide()
+	
+func _on_idle_timer_timeout():
+	# Play different idle according to direction
+	$AnimationPlayer.play("idle")
